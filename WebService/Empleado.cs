061c5e4dca7b9e428bcb9340f0d79a62bd3f855a
AleKiller21 <aleferrera@hotmail.com>
2015-08-17 09:43:01 -0600

    Creacion de clase empleado

---
+++ WebService/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebService.MirrorClasses;
+
+namespace WebService
+{
+    public class Empleado
+    {
+        public UserMirror User { get; set; }
+        public List<DepartamentoMirror> Departamento { get; set; }
+        public List<JerarquiaMirror> Jerarquias { get; set; }
+        public List<LogVacacionesMirror> LogVacaciones { get; set; }
+        public List<PermisosMirror> Permisos { get; set; }
+        public List<RolesMirror> Roles { get; set; }
+        public List<StatusMirror> Status { get; set; }
+        public List<TipoDiaMirror> TipoDia { get; set; }
+        public List<VacacionesMirror> Vacaciones { get; set; }
+    }
+}
